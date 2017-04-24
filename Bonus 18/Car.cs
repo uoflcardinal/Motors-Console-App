b7b321f9e1c151d12bfe4bb87bd31355ef2c5d54
Brian Wood <uoflcardinalsalltheway@yahoo.com>
2017-04-24 01:31:48 -0400

    Grand Circus Motors Admin App

---
+++ Bonus 18/Car.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus_18
+{
+    class Car
+    {
+        #region Variables
+        private string make;
+        private string model;
+        private int year;
+        private decimal price;
+        #endregion
+
+        #region Properties
+        public string Make
+        {
+            get
+            {
+                return make;
+            }
+
+            set
+            {
+                make = value;
+            }
+
+        }
+
+        public string Model
+        {
+            get
+            {
+                return model;
+            }
+
+            set
+            {
+                model = value;
+            }
+        }
+
+        public int Year
+        {
+            get
+            {
+                return year;
+            }
+
+            set
+            {
+                year = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                return price;
+            }
+
+            set
+            {
+                price = value;
+            }
+        }
+        #endregion
+
+        #region Constructors
+        public Car(string MakeInput, string ModelInput, int YearInput, decimal PriceInput)
+        {
+            make = MakeInput;
+            model = ModelInput;
+            year = YearInput;
+            price = PriceInput;
+        }
+
+        public Car()
+        {
+            make = "Ford";
+            model = "Mustang";
+            year = 1970;
+            price = 25000.00m;
+        }
+        #endregion
+    }
+}
---
+++ Bonus 18/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numOfCars;
+            string Make;
+            string Model;
+            int Year;
+            decimal Price;
+
+
+            Console.WriteLine("Welcome to the Grand Circus Motors admin console!\n\n");
+
+            Console.Write("How many cars are you entering: ");
+            numOfCars = int.Parse(Console.ReadLine());
+
+            int[] newCars = new int[numOfCars];
+
+
+
+            for (int i = 1; i <= numOfCars; i++)
+            {
+                Console.WriteLine("Enter Car #{0} Make: ", i);
+                Make = Console.ReadLine();
+                Console.WriteLine("Enter Car #{0} Model: ", i);
+                Model = Console.ReadLine();
+                Console.WriteLine("Enter Car #{0} Year: ", i);
+                Year = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Car #{0} Price: ", i);
+                Price = decimal.Parse(Console.ReadLine());
+
+                Car c = new Car(Make, Model, Year, Price);
+
+                Console.WriteLine("Current Inventory:");
+                Console.WriteLine("{0}\t\t{1}\t\t{2}\t\t{3}\t\t", c.Make, c.Model, c.Year, c.Price);
+            }          
+        }
+    }
+}
